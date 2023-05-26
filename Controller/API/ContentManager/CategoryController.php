fcbbffb0816d016604e3e9e8fbbf7b9ee27aae1c
zeriix <willflo46@gmail.com>
2023-05-26 17:44:11 +0200

    [ADD]: Adding base controller - [EDIT]: Change paramter and edit getters/setters Category entity

---
+++ Controller/API/ContentManager/CategoryController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace controller\API\ContentManager\CategoryController;
+
+use Core\Controller;
+use Core\Request;
+use Core\Response;
+
+use Entity\Category;
+use Services\Back\CategoryManagerService as CategoryManager;
+
+// Valid
+class createCategory extends Controller
+{
+    public function checkers(Request $request): array
+    {
+        return [
+            ["category/name", $request->getBody()['name']],
+        ];
+    }
+
+    public function handler(Request $request, Response $response): void
+    {
+        try {
+            $categoryManager = new CategoryManager();
+
+            $categoryManager->createCategory(
+                $this->floor->pickup("category/name")
+            );
+
+            $response->send(["message" => "Category created"]);
+        } catch (\Exception $e) {
+            $response->send(["error" => $e->getMessage()]);
+        }
+    }
+}
+// Valid
+class getCategories extends Controller
+{
+    public function checkers(Request $request): array
+    {
+        return [];
+    }
+
+    public function handler(Request $request, Response $response): void
+    {
+        try {
+            $categoryManager = new CategoryManager();
+
+            $categories = $categoryManager->getCategories();
+
+            $response->send(["categories" => $categories]);
+        } catch (\Exception $e) {
+            $response->send(["error" => $e->getMessage()]);
+        }
+    }
+}
+// Valid
+class deleteCategory extends Controller
+{
+    public function checkers(Request $request): array
+    {
+        return [
+            ["category/name", $request->getBody()['name']],
+        ];
+    }
+
+    public function handler(Request $request, Response $response): void
+    {
+        try {
+            $categoryManager = new CategoryManager();
+
+            $categoryManager->deleteCategory(
+                $this->floor->pickup("category/name")
+            );
+
+            $response->send(["message" => "Category deleted"]);
+        } catch (\Exception $e) {
+            $response->send(["error" => $e->getMessage()]);
+        }
+    }
+}
+// Valid
+class getAllContentWhereCategory extends Controller
+{
+    public function checkers(Request $request): array
+    {
+        return [
+            ["category/name", $request->getQuery('name')],
+        ];
+    }
+
+    public function handler(Request $request, Response $response): void
+    {
+        try {
+            $category = Category::findFirst(["title" => $this->floor->pickup("category/name")]);
+            $videos = $category->getVideos();
+            $content = [];
+            foreach ($videos as $video) {
+                if ($video->getFilm() != null) {
+                    $film = $video->getFilm();
+                    $content[] = [
+                        "id_film" => $film->getId(),
+                        "title" => $film->getVideo()->getTitle(),
+                        "description" => $film->getVideo()->getDescription(),
+                        "created_at" => $film->getCreatedAt(),
+                        "updated_at" => $film->getUpdatedAt()
+                    ];
+                } else if ($video->getSeries() != null) {
+                    $episodes = $video->getSeries();
+                    foreach ($episodes as $episode) {
+                        if (!in_array($episode->getTitle(), array_column($content, "title"))) {
+                            $content[] = [
+                                "id_serie" => $episode->getId(),
+                                "title" => $episode->getTitle(),
+                                "image" => $episode->getImage(),
+                                "created_at" => $episode->getCreatedAt(),
+                                "updated_at" => $episode->getUpdatedAt()
+                            ];
+                        }
+                    }
+                } else {
+                    throw new \Exception("Error getting content");
+                }
+            }
+
+            $response->send(["content" => $content]);
+        } catch (\Exception $e) {
+            $response->send(["error" => $e->getMessage()]);
+        }
+    }
+}
