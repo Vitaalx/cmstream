dff857727b9e10e1e2086ce29c8caa3b6bf8fa26
Maubry94 <matthieu.aubry94370@gmail.com>
2023-05-31 18:48:28 +0200

    [UPDATE] back

--- Template/back.php
+++ Template/back.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="/public/cuteVue.js"></script>
+    <script src="/public/assets/js/cuteVue.js"></script>
 </head>
 <body>
     <h1>template back</h1>
