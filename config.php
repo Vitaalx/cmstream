8fc3f76d9940411cf137106698f1449b388bff00
mathcovax <campani.mathieu@gmail.com>
2023-05-31 17:59:45 +0100

    [fix] index && config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("CONFIG", [
+    'DB_HOST' => 'database',
+    'DB_PORT' => '5432',
+    'APP_NAME' => 'cmStream',
+    'DB_CONNECTION' => 'pgsql',
+    'DB_DATABASE' => 'esgi',
+    'DB_USERNAME' => 'esgi',
+    'DB_PASSWORD' => 'Test1234',
+    'SECRET_KEY' => 'XXXXXX',
+    'TOKEN_DURATION' => 3600
+]);
