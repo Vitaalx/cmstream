5f165ddab2d10e134308020ba48baf579a3ddb3b
Maubry94 <matthieu.aubry94370@gmail.com>
2023-06-01 10:49:12 +0200

    [UPDATE] add header account options

--- Template/front.php
+++ Template/front.php
@@ -61,13 +61,11 @@
                         </a>
                     </li>
                     <li class="relative h-full">
-                        <a class="h-full px-[10px] flex items-center text-sm text-whiteless hover:text-white hover:bg-blackless" href="#">
-                            <span class="mdi mdi-account-circle text-[24px]"></span>
-                        </a>
+                        <div id="accountButtonCtnr"></div>
                     </li>
                 </ul>
             </nav>
-            <div id="burgerMenuCtnr"></div>
+            <div id="burgerButtonCtnr"></div>
         </div>
         <div id="menuWrapper" class="z-5 menu-wrapper">
             <nav>
@@ -100,14 +98,14 @@
             
 	    </div>
     </header>
-    <div id="page" class="pt-[60px]">
+    <div id="page" class="pt-[60px] min-h-[calc(100vh-60px)]">
         <?php include $view ?>   
     </div>
     <footer class="px-14 py-12 bg-darkblue">
         <div class="max-w-[1050px] mx-auto footer-content">
             <div class="top flex flex-col lg:flex-row justify-between">
                 <nav class="navigation-nav">
-                    <h2 class="text-xl font-bold leading-normal mt-0 mb-2 text-white">navigation</h2>
+                    <h2 class="text-xl font-bold leading-normal mt-0 mb-2 text-white">Navigation</h2>
                     <ul>
                         <li>
                             <a class="text-sm text-whiteless hover:text-white" href="#">Catalogue</a>
@@ -192,10 +190,75 @@
                 burgerButton.classList.toggle("active");
                 menuWrapper.classList.toggle("active");
             }
-        },
+        }
     });
 
-    burgerButton.mount("#burgerMenuCtnr");
+    burgerButton.mount("#burgerButtonCtnr");
+</script>
 
+<div style="display:none">
+    <div id="accountButton" class="relative h-full">
+        <div class="h-full px-[10px] flex items-center text-sm text-whiteless cursor-pointer hover:text-white hover:bg-blackless" cv-class="{ 'bg-blackless': this.displayOptionsMenu }" @click="this.displayOptions()">
+            <span class="mdi mdi-account-circle text-[24px]" cv-class="{ 'text-white': this.displayOptionsMenu }"></span>
+            <span cv-if="!this.displayOptionsMenu" class="mdi mdi-triangle-small-down"></span>
+            <span cv-if="this.displayOptionsMenu" class="mdi mdi-triangle-small-up text-white"></span>
+        </div>
+        <div cv-if="this.displayOptionsMenu" class="absolute top-[60px] right-0 w-[280px] bg-blackless account-options">
+            <div cv-if="!this.connected" class="py-4 flex flex-col gap-4">
+                <div class="px-4 py-2 flex flex-col text-whiteless hover:bg-darkblue signup">
+                    <span class="text-xl font-semibold">Créer un compte</span>
+                    <p class="text-sm">Inscrivez-vous gratuitement.</p>
+                </div>
+                <div class="px-4 py-2 flex flex-col text-whiteless hover:bg-darkblue signin">
+                    <span class="text-xl font-semibold">Se connecter</span>
+                    <p class="text-sm">Déjà inscrit ? Bon retour.</p>
+                </div>
+            </div>
+            <div cv-if="this.connected" class="py-4 flex flex-col gap-4">
+                <div class="px-4 py-2 flex gap-[10px] items-center text-whiteless hover:bg-darkblue user">
+                    <span class="mdi mdi-account-circle text-[40px]"></span>
+                    <span class="font-semibold">Username</span>
+                </div>
+                <div class="h-[2px] bg-darkblue separator"></div>
+                <div class="options">
+                    <div class="px-4 pt-2 pb-1 flex gap-[10px] items-center text-whiteless hover:text-white hover:bg-darkblue notifications">
+                        <span class="mdi mdi-bell-outline text-[24px]"></span>
+                        <span>Notifications</span>
+                    </div>
+                    <div class="px-4 pt-1 pb-2 flex gap-[10px] items-center text-whiteless hover:text-white hover:bg-darkblue account">
+                        <span class="mdi mdi-account text-[24px]"></span>
+                        <span>Paramètres du compte</span>
+                    </div>
+                </div>
+                <div class="h-[2px] bg-darkblue separator"></div>
+                <div class="p-4 flex gap-[10px] items-center text-whiteless hover:text-white hover:bg-darkblue disconnect">
+                    <span class="mdi mdi-logout text-[24px]"></span>
+                    <span>Se déconnecter</span>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+<script>
+    const accountButton = new CuteVue({
+        el: "#accountButton",
+        data: {
+            connected: false,
+            displayOptionsMenu: false
+        },
+        props: {},
+        mounted(){},
+        unmounted(){},
+        methods: {
+            displayOptions(){
+                this.displayOptionsMenu = !this.displayOptionsMenu;
+                console.log(this.displayOptionsMenu);
+            }
+        }
+    });
+
+    accountButton.mount("#accountButtonCtnr");
 </script>
 </html>
