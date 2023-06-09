42637b69ac25f5adddb3dce94bf4b05ea6895279
mathcovax <campani.mathieu@gmail.com>
2023-06-09 18:17:42 +0100

    [fix] Entity set null

--- Core/Entity.php
+++ Core/Entity.php
@@ -194,8 +194,8 @@ abstract class Entity implements \JsonSerializable
     protected function set(string $prop, mixed $value): void
     {
         $prop = self::$reflections[static::class][$prop];
-        if ($prop["entityProp"] !== null && $value !== null) {
-            $this->props[$prop["name"] . "_id"] = $value->getId();
+        if ($prop["entityProp"] !== null){
+            $this->props[$prop["name"] . "_id"] = $value !== null ? $value->getId() : null;
         } else $this->props[$prop["name"]] = $value;
     }
 
