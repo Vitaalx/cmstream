e05248fd89cf2fe05df63463179e0bb7febab429
Maubry94 <matthieu.aubry94370@gmail.com>
2023-06-11 19:29:56 +0200

    [INIT] : admin front part

--- Controller/SPA/front.php
+++ Controller/SPA/front.php
@@ -17,6 +17,7 @@ class index extends IndexHandler{}
  * @GET{/signup}
  * @GET{/validate}
  * @GET{/forgot-password}
+ * @GET{/admin} // TODO: move to connected class when admin panel will be ready
  */
 class guest extends IndexHandler{
     public function checkers(Request $request): array
