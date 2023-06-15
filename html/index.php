6a3618bee5ae54a2f2f7d93e59c77f86941c2382
mathcovax <campani.mathieu@gmail.com>
2023-06-15 11:42:09 +0100

    [feat] permissions

--- html/index.php
+++ html/index.php
@@ -1,19 +1,20 @@
 <?php
 # ROUTE CMSTREAM
 require __DIR__ . "/../Core/Route.php";
-require __DIR__ . "/generate.index.php";
 use Core\Route;
-
-Route::match([
-    "method" => "*",
-    "path" => "/api/.*",
-    "controller" => "handlers/notfound",
-]);
 
 Route::match([
     "method" => "GET",
     "path" => "/public/.*",
     "controller" => "handlers/assets",
+]);
+
+require __DIR__ . "/generate.index.php";
+
+Route::match([
+    "method" => "*",
+    "path" => "/api/.*",
+    "controller" => "handlers/notfound",
 ]);
 
 Route::match([
