962414a0bd589cdc633af00d81d8e4a422880cad
Maubry94 <matthieu.aubry94370@gmail.com>
2023-06-21 18:47:51 +0200

    [ADD]: user store avatarColor

--- public/cuteVue/user.js
+++ public/cuteVue/user.js
@@ -13,6 +13,7 @@ export const userStore =  CuteVue.createStore(
             firstname: "",
             email: "",
             isConnected: false,
+            avatarColor: ""
         },
         actions: {
             async connect(){
@@ -32,6 +33,37 @@ export const userStore =  CuteVue.createStore(
                 this.role = "";
                 this.isConnected = false;
                 await taob.get("/logout").result;
+            }
+        },
+        computed: {
+            randomColor() {
+                const excludedColors = [
+                  [255, 255, 255], // Blanc
+                  [0, 0, 0], // Noir
+                ];
+                const lightnessThreshold = 150;
+                let randomColor = [];
+                let isColorInvalid = true;
+              
+                while (isColorInvalid) {
+                  randomColor = [
+                    Math.floor(Math.random() * 256),
+                    Math.floor(Math.random() * 256),
+                    Math.floor(Math.random() * 256),
+                  ];
+              
+                  const brightness = (randomColor[0] * 299 + randomColor[1] * 587 + randomColor[2] * 114) / 1000;
+              
+                  const isExcludedColor = excludedColors.some(color =>
+                    color[0] === randomColor[0] &&
+                    color[1] === randomColor[1] &&
+                    color[2] === randomColor[2]
+                  );
+              
+                  isColorInvalid = brightness > lightnessThreshold || isExcludedColor;
+                }
+              
+                this.avatarColor = `rgb(${randomColor.join(",")})`;
             }
         }
     }
