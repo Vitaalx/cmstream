ff64d8d52fffe60fdcefb630eae04e778247ab4c
zeriix <willflo46@gmail.com>
2023-06-30 18:45:44 +0200

    [EDIT] : modify access file Core/File

--- Core/File.php
+++ Core/File.php
@@ -5,62 +5,57 @@ namespace Core;
 class File
 {
     private string $path;
-    private mixed $file;
 
     public function __construct(string $path)
     {
         $this->path = $path;
-        $this->file = fopen($path, "a+");
     }
 
     public function read()
     {
-        fseek($this->file, 0, SEEK_SET);
-        $content = fread($this->file, filesize($this->path));
+        $file = fopen($this->path, "r");
+        fseek($file, 0, SEEK_SET);
+        $content = fread($file, filesize($this->path));
         return $content;
     }
 
     public function write($data): File
     {
-        fseek($this->file, 0, SEEK_SET);
-        fwrite($this->file, $data);
+        $file = fopen($this->path, "w");
+        fseek($file, 0, SEEK_SET);
+        fwrite($file, $data);
         return $this;
     }
 
     public function append($data): File
     {
-        fseek($this->file, 0, SEEK_END);
-        fwrite($this->file, $data);
+        $file = fopen($this->path, "a+");
+        fseek($file, 0, SEEK_END);
+        fwrite($file, $data);
         return $this;
     }
 
     public function rename($path): File
     {
-        fclose($this->file);
         rename($this->path, $path);
         $this->path = $path;
-        $this->file = fopen($path, "a+");
         return $this;
     }
 
     public function copyTo($path): File
     {
-        fclose($this->file);
         copy($this->path, $path);
         $this->path = $path;
-        $this->file = fopen($path, "a+");
         return $this;
     }
 
     public function delete(): void
     {
-        fclose($this->file);
-        $this->file = null;
         unlink($this->path);
     }
 
-    public function __destruct()
+    public function getSize(): int
     {
-        if ($this->file !== null && $this->file !== false) fclose($this->file);
+        return filesize($this->path);
     }
 }
