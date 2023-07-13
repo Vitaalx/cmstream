ff4878b788d95de92fbc9f93d4179806de50dd36
Maubry94 <matthieu.aubry94370@gmail.com>
2023-07-13 15:39:36 +0200

    [FIX]: votes

--- Checker/category.php
+++ Checker/category.php
@@ -9,7 +9,7 @@ use Entity\Category;
 function name(string $name, Floor $floor, Response $response): string
 {
     $name = trim($name);
-    if (strlen($name) < 4 || strlen($name) > 20) {
+    if (strlen($name) < 3 || strlen($name) > 20) {
         $response->info("category.name")->code(400)->send();
     }
     return $name;
